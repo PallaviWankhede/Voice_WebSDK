b3656d84bced997008dda75d0918754412b21d30
PallaviWankhede <89701574+PallaviWankhede@users.noreply.github.com>
2025-01-10 10:56:53 +0530

    Update app.js

--- Kore-sdk-main/Kore-sdk-main/UI/app.js
+++ Kore-sdk-main/Kore-sdk-main/UI/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const brand_names = require('./TW_Master_json');
-const fourW_brand_names = require('./4W_Master_json');
+// const fourW_brand_names = require('./4W_Master_json');
 const router = express.Router();
 
 router.get('/tw-brands', (req,res)=>{
